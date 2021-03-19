017bbabd47737c61c5f5e7303c7a1854eb328494
Michael <Mlcorrieri@gmail.com>
2021-03-19 18:14:43 -0400

    array find done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// function superbowlWin(records) {
+//   function getW(arrVal) {
+//     const result = arrVal.result === "W";
+//     if (result) {
+//       const year = records.find(getW).year;
+//       return year;
+//     }
+//   }
+// }
 
+// superbowlWin(records);
+
+function superbowlWin(array) {
+  const wonYear = array.find((index) => index.result === "W");
+  if (wonYear) {
+    return wonYear.year;
+  }
+}
